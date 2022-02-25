5872b74f9307d00a09cbc874b0315563b63ab367
nandabritto <nanda.britto@gmail.com>
2022-02-25 23:07:53 +0000

    Add lal and lng on for loop

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -12,52 +12,78 @@ http.open('get', 'places.json', true);
 
 http.send();
 
-http.onload = function(){
-  if(this.readyState == 4 && this.status == 200){
+function initMap() {}
+
+http.onload = function () {
+  if (this.readyState == 4 && this.status == 200) {
     let places = JSON.parse(this.responseText);
 
     let output = "";
 
-    for (let place of places){
-      output +=`
+    for (let place of places) {
+      output += `
         <div class="places">
         <p>${place.place}</p>
         <img src="${place.image}" alt="${place.image}">
-        <p class="description">${place.description}</p>
-        <div id="map">initMap()</div>       
+        <p class="description">${place.description}</p>    
+        <div id='map_${place.place}' class="map"></div>
         </div>  
       `;
+      
+    //   var map = new google.maps.Map(document.getElementById('map'), {
+    //     center: {
+    //         lat: parseFloat(place.lat),
+    //         lng: parseFloat(place.lng),
+    //     },
+    //     zoom: 15,
+    //     mapTypeId: "satellite",
+    // });
     }
     document.querySelector(".places").innerHTML = output;
 
+
+  for (let place of places) {
+    mapid="map_" + place.place
+  var map = new google.maps.Map(document.getElementById(mapid), {
+    center: {
+        lat: parseFloat(place.lat),
+        lng: parseFloat(place.lng),
+    },
+    zoom: 15,
+    mapTypeId: "satellite",
+  });
   }
+
+
+}
 }
 
+// let map;
+
+// // fetch('./places.json')
+// //   .then((response) => {
+// //     return response.json()
+// //   })
+// //   .then((data) => {
+// //     // Work with JSON data here
+// //     console.log(data)
+// //   })
 
 
-let map;
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: {
+//       lat: parseFloat(place.lat),
+//       lng: parseFloat(place.lng),
+//     },
+//     zoom: 5,
 
-fetch('./places.json')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    // Work with JSON data here
-    console.log(data)
-  })
-  
-  function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-      center: {
-        lat: data.lat,
-        lng: data.lng
-      },
-      zoom: 15,
-      mapTypeId: "satellite",
-  
-    });
-  };
-  
+//     mapTypeId: "satellite",
+
+//   });
+// };
+
+
 
 
 
