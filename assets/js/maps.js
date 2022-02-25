ea6ead0e7e46721c0faf6b722d9175aa46dca901
nandabritto <nanda.britto@gmail.com>
2022-02-25 19:06:51 +0000

    Add contants and dict on js file

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,11 +1,36 @@
+const placeElement = document.getElementById('place');
+const placeDescriptionElement = document.getElementById('place-description');
+
+function showPlace(pPlace) {
+  placeElement.innerText = pPlace.place;
+  placeDescriptionElement.innerText = pPlace.description;
+};
+
 let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 48.858525392116746, lng: 2.294530492488468},
-    zoom: 17,
+    center: {
+      lat: parseFloat(placesBank.lat),
+      lng: parseFloat(placesBank.lat)
+    },
+    zoom: 15,
     mapTypeId: "satellite",
-    rotateControl: true
 
   });
-}
+};
+
+
+const placesBank = [{
+  place: "Paris",
+  description: "test",
+  lat: '40.822633855656974',
+  lng: '14.425354516052282',
+  mark: "Mount Vesuvius"
+},
+{
+  place: "Rio de Janeiro",
+  lat: '40.822633855656974',
+  lng: '14.425354516052282',
+  mark: "Rio de Janeiro"
+}];
