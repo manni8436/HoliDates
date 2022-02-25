750ea0be81ff4a951736b6c1f02d5bae6cf511ac
nandabritto <nanda.britto@gmail.com>
2022-02-25 21:01:53 +0000

    Create a fetch request to get json data

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,36 +1,77 @@
-const placeElement = document.getElementById('place');
-const placeDescriptionElement = document.getElementById('place-description');
+// const placeElement = document.getElementById('place');
+// const placeDescriptionElement = document.getElementById('place-description');
 
-function showPlace(pPlace) {
-  placeElement.innerText = pPlace.place;
-  placeDescriptionElement.innerText = pPlace.description;
-};
+// function showPlace(pPlace) {
+//   placeElement.innerText = pPlace.place;
+//   placeDescriptionElement.innerText = pPlace.description;
+// };
+
+let http = new XMLHttpRequest();
+
+http.open('get', 'places.json', true);
+
+http.send();
+
+http.onload = function(){
+  if(this.readyState == 4 && this.status == 200){
+    let places = JSON.parse(this.responseText);
+
+    let output = "";
+
+    for (let place of places){
+      output +=`
+        <div class="places">
+        <p>${place.place}</p>
+        <img src="${place.image}" alt="${place.image}">
+        <p class="description">${place.description}</p>
+        <div id="map">initMap()</div>       
+        </div>  
+      `;
+    }
+    document.querySelector(".places").innerHTML = output;
+
+  }
+}
+
+
 
 let map;
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: {
-      lat: parseFloat(placesBank.lat),
-      lng: parseFloat(placesBank.lat)
-    },
-    zoom: 15,
-    mapTypeId: "satellite",
+fetch('./places.json')
+  .then((response) => {
+    return response.json()
+  })
+  .then((data) => {
+    // Work with JSON data here
+    console.log(data)
+  })
+  
+  function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+      center: {
+        lat: data.lat,
+        lng: data.lng
+      },
+      zoom: 15,
+      mapTypeId: "satellite",
+  
+    });
+  };
+  
 
-  });
-};
 
 
-const placesBank = [{
-  place: "Paris",
-  description: "test",
-  lat: '40.822633855656974',
-  lng: '14.425354516052282',
-  mark: "Mount Vesuvius"
-},
-{
-  place: "Rio de Janeiro",
-  lat: '40.822633855656974',
-  lng: '14.425354516052282',
-  mark: "Rio de Janeiro"
-}];
+// const placesBank = [{
+//   place: "Paris",
+//   description: "test",
+//   image
+//   lat: '40.822633855656974',
+//   lng: '14.425354516052282',
+//   mark: "Mount Vesuvius"
+// },
+// {
+//   place: "Rio de Janeiro",
+//   lat: '40.822633855656974',
+//   lng: '14.425354516052282',
+//   mark: "Rio de Janeiro"
+// }];
