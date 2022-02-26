6acc950e67274e162cbbb5a30828c9478dd97f02
Raivis80 <rapet80@gmail.com>
2022-02-26 21:22:33 +0000

    fixed hero content position and added js basic script

---
+++ assets/js/floatingText.js
@@ -0,0 +1,21 @@
+
+var quotes = [
+    "hello i am the first title",
+    "whats up, i am the second title",
+    "add here any extra quotes you want"
+];
+
+textSequence(0);
+function textSequence(i) {
+
+    if (quotes.length > i) {
+        setTimeout(function () {
+            document.getElementById("quotes").innerHTML = quotes[i];
+            textSequence(++i);
+        }, 3000);
+
+    } else if (quotes.length == i) {
+        textSequence(0);
+    }
+
+}
