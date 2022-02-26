9a41878c0084c234085947ec2df46f14b813f48a
nandabritto <nanda.britto@gmail.com>
2022-02-26 15:14:32 +0000

    Add markers to google maps

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -2,11 +2,42 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 48.24714034374518, lng: 11.607449487355048 },
-    zoom: 4,
-    mapTypeId: google.maps.MapTypeId.HYBRID
+      center: {
+        lat: 48.24714034374518,
+        lng: 11.607449487355048
+      },
+      zoom: 4,
+      mapTypeId: google.maps.MapTypeId.HYBRID
+    
   });
+
+  let http = new XMLHttpRequest();
+
+  http.open('get', 'places.json', true);
+
+  http.send();
+
+  http.onload = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      let places = JSON.parse(this.responseText);
+
+      for (let place of places) {
+        const coord = {
+          lat: place.lat,
+          lng: place.lng          
+        };
+        
+
+        const marker = new google.maps.Marker({
+          position: coord,
+          map: map,
+          title: 'Hello World!'
+        });
+     }
+    };
+  }
 }
+
 
 
 
@@ -50,4 +81,3 @@ function initMap() {
 //   });
 //   }
 // }
-
