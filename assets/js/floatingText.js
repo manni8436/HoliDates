31ba23152268441835e5ca59103fd4d2ba881b76
Alexa Hendry <alexahendry88@gmail.com>
2022-02-27 18:36:49 +0000

    Add quotes content

--- assets/js/floatingText.js
+++ assets/js/floatingText.js
@@ -1,8 +1,10 @@
 
 var quotes = [
-    "hello i am the first title",
-    "whats up, i am the second title",
-    "add here any extra quotes you want"
+    "We just loved Bruges, our weekend away was magical!",
+    "Everyone says it constantly rains in Ireland - they couldn't be more wrong!",
+    "Venice was a dream come true, we couldn't have hoped for a better honeymoon!",
+    "I couldn't believe how smelly Paris was...definitely not going back there!",
+    "The highlight in Rome for me was the Colosseum - it was breathtaking!"
 ];
 
 textSequence(0);
